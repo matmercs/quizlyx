84982bc2a74dd1d30b35dd0305bbfd1f541033f9
Matthew <mateogrigio@gmail.com>
2026-03-05 18:34:20 +0300

    time provider

---
+++ server/interfaces/itime_provider.hpp
@@ -0,0 +1,18 @@
+#ifndef QUIZLYX_SERVER_INTERFACES_ITIME_PROVIDER_HPP
+#define QUIZLYX_SERVER_INTERFACES_ITIME_PROVIDER_HPP
+
+#include <chrono>
+
+namespace quizlyx::server::interfaces {
+
+class ITimeProvider {
+ public:
+  virtual ~ITimeProvider() = default;
+
+  virtual std::chrono::steady_clock::time_point Now() const = 0;
+};
+
+}  // namespace quizlyx::server::interfaces
+
+#endif  // QUIZLYX_SERVER_INTERFACES_ITIME_PROVIDER_HPP
+

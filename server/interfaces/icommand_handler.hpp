78557a62827c63375cfc7892bcbc06b76f79e487
Matthew <mateogrigio@gmail.com>
2026-03-05 18:38:14 +0300

    test basic build

---
+++ server/interfaces/icommand_handler.hpp
@@ -0,0 +1,39 @@
+#ifndef QUIZLYX_SERVER_INTERFACES_ICOMMAND_HANDLER_HPP
+#define QUIZLYX_SERVER_INTERFACES_ICOMMAND_HANDLER_HPP
+
+#include <optional>
+#include <string>
+
+#include "domain/answer.hpp"
+#include "domain/quiz.hpp"
+
+namespace quizlyx::server::interfaces {
+
+struct SessionCreated {
+  std::string session_id;
+  std::string pin;
+};
+
+class ICommandHandler {
+ public:
+  virtual ~ICommandHandler() = default;
+
+  virtual std::optional<std::string> CreateQuiz(domain::Quiz quiz) = 0;
+
+  virtual std::optional<SessionCreated> CreateSession(const std::string& quiz_code,
+                                                      const std::string& host_id) = 0;
+
+  virtual bool StartGame(const std::string& session_id) = 0;
+  virtual bool NextQuestion(const std::string& session_id) = 0;
+
+  virtual bool JoinAsPlayer(const std::string& pin, const std::string& player_id) = 0;
+  virtual bool LeaveSession(const std::string& session_id, const std::string& player_id) = 0;
+
+  virtual bool SubmitAnswer(const std::string& session_id, const std::string& player_id,
+                            const domain::PlayerAnswer& answer) = 0;
+};
+
+}  // namespace quizlyx::server::interfaces
+
+#endif  // QUIZLYX_SERVER_INTERFACES_ICOMMAND_HANDLER_HPP
+
--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,47 @@
+#include <chrono>
+
+#include "interfaces/ibroadcast_sink.hpp"
+#include "interfaces/itime_provider.hpp"
+#include "services/in_memory_quiz_storage.hpp"
+#include "services/quiz_registry.hpp"
+#include "services/session_manager.hpp"
+#include "services/session_timer_service.hpp"
+
+namespace quizlyx::server {
+
+class SteadyTimeProvider : public interfaces::ITimeProvider {
+ public:
+  std::chrono::steady_clock::time_point Now() const override {
+    return std::chrono::steady_clock::now();
+  }
+};
+
+class NoOpBroadcastSink : public interfaces::IBroadcastSink {
+ public:
+  void Broadcast(const std::string& /*session_id*/,
+                 const ::quizlyx::server::events::GameEvent& /*event*/) override {}
+};
+
+}  // namespace quizlyx::server
+
 int main(int argc, char** argv) {
   (void)argc;
   (void)argv;
+
+  using namespace quizlyx::server;
+
+  services::InMemoryQuizStorage quiz_storage;
+  services::QuizRegistry quiz_registry{quiz_storage};
+
+  NoOpBroadcastSink broadcast_sink;
+  SteadyTimeProvider time_provider;
+
+  services::SessionManager session_manager{quiz_registry, broadcast_sink};
+  services::SessionTimerService timer_service{time_provider, std::chrono::milliseconds{100}};
+
+  (void)session_manager;
+  (void)timer_service;
+
   return 0;
 }
+
